4544e946147d43852ad753cd21280fe0a5def7d7
Cody Quinn <codyquinn1122@gmail.com>
2020-06-28 12:38:38 -0700

    Added background images

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, url_for, redirect
+import random
 
 app = Flask(__name__)
 
@@ -6,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/api/picture')
+def picture():
+    return redirect(url_for('static', filename='img/bg-home/image{}.jpg'.format(random.randint(1,5))))
