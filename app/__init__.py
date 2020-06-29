1eb088c72713a938fc1cb474ef694bde078f5cfb
Cody Quinn <codyquinn1122@gmail.com>
2020-06-29 15:48:28 -0700

    Updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -21,14 +21,11 @@ def picture():
     return redirect(url_for('static', filename='img/bg-home/image{}.jpg'.format(random.randint(1,5))))
 
 
-@app.route('/api/auth/mc-plugins/{}')
-def auth_mcplugins(uid):
-    if uid == '65dc2c59-2e02-4e6b-9be1-e73c85ce0cdc':
-        return {
-            'uuid': '65dc2c59-2e02-4e6b-9be1-e73c85ce0cdc',
-            'name': 'CustodyShop',
-            'enabled': True,
-            'paid': False
-        }
-    else:
-        return {}
+@app.route('/api/auth/mc-plugins/65dc2c59-2e02-4e6b-9be1-e73c85ce0cdc/')
+def auth_mcplugins():
+    return "{\
+        'uuid': '65dc2c59-2e02-4e6b-9be1-e73c85ce0cdc',\
+        'name': 'CustodyShop',\
+        'enabled': True,\
+        'paid': False\
+    }"
