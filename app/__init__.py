42ede4f7c707b71bb33e46cef5563d96b451aa0e
Cody Quinn <codyquinn1122@gmail.com>
2020-06-29 15:28:08 -0700

    Added MCAuth api

--- app/__init__.py
+++ app/__init__.py
@@ -19,3 +19,16 @@ def home():
 @app.route('/api/picture')
 def picture():
     return redirect(url_for('static', filename='img/bg-home/image{}.jpg'.format(random.randint(1,5))))
+
+
+@app.route('/api/auth/mc-plugins/{}')
+def auth_mcplugins(uid):
+    if uid == '65dc2c59-2e02-4e6b-9be1-e73c85ce0cdc':
+        return {
+            'uuid': '65dc2c59-2e02-4e6b-9be1-e73c85ce0cdc',
+            'name': 'CustodyShop',
+            'enabled': True,
+            'paid': False
+        }
+    else:
+        return {}
