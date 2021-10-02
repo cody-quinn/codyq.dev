d3f7abc9a34e1f8fabd5a4c59df9ab3a5ee82b67
CatDevz <codyquinn1122@gmail.com>
2021-10-02 11:52:17 -0500

    :chart_with_upwards_trend: Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import "../styles/globals.css";
 import "highlight.js/styles/atom-one-dark.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+
 function MyDocument() {
   return (
     <Html>
       <Head>
         <link rel='icon' href='data:;base64,=' />
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
