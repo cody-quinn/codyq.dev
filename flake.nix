cc7eb749e891fbcd30890c4a73a6b712a2c71664
Cody <cody@codyq.dev>
2025-05-29 14:11:05 -0500

    Updated flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -18,7 +18,7 @@
           packages = with pkgs; [
             dotnet-sdk_8
             libgit2
-            nodePackages.wrangler
+            wrangler
           ];
           DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
           LD_LIBRARY_PATH = "${pkgs.libgit2}/lib";
